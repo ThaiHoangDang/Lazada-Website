b8da7682613b99e80bf0a0a28d803ddf05b57987
Dau Hoang Minh <s3926201@rmit.edu.vn>
2022-08-26 22:52:39 +0700

    Form validate + clean code

--- js/modal/profile.js
+++
@@ -1,7 +0,0 @@
-var loadFile = function (event) {
-    var image = document.getElementById("profile-img");
-    image.src = URL.createObjectURL(event.target.files[0]);
-    image.onload = function () {
-        URL.revokeObjectURL(output.src)
-    }
-};
