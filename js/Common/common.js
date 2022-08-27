90c5cc7588581a64953ea2a9bf712c7736b43a1e
Dau Hoang Minh <s3926201@rmit.edu.vn>
2022-08-27 16:20:31 +0700

    fix Validate + minor

--- js/Common/common.js
+++ js/Common/common.js
@@ -1,23 +1,41 @@
+// Show the image input 
 var loadFile = function (event) {
-    var image = document.getElementById("img-output");
+    var image = document.querySelector('#img-output');
     image.src = URL.createObjectURL(event.target.files[0]);
     image.onload = function () {
         URL.revokeObjectURL(output.src)
     }
 };
+function checkErrorMessage() {
+    let pass = document.querySelector('#password').value;
+    let feedback = document.querySelector('#password-feedback');
+    if (!/^[A-Za-z\d!@#$%^&*]{8,20}$/.test(pass)) {
+        feedback.innerHTML = "Password must be from 8 to 20 characters with appropriate characters"
+    } else if (!/(?=.*[a-z])/.test(pass)) {
+        feedback.innerHTML = "Password must contain at least 1 lowercase character"
+    } else if (!/(?=.*[A-Z])/.test(pass)) {
+        feedback.innerHTML = "Password must contain at least 1 uppercase character"
+    } else if (!/(?=.*\d)/.test(pass)) {
+        feedback.innerHTML = "Password must contain at least 1 digit"
+    } else if (!/(?=.*[!@#$%^&*])/.test(pass)) {
+        feedback.innerHTML = "Password must contain at least 1 special character: !@#$%^&*"
+    }
+}
+// Validate Bootstrap form
+(() => {
+    'use strict'
+    const forms = document.querySelectorAll('.needs-validation')
 
-(function () {
-    'use strict';
-    window.addEventListener('load', function () {
-        var forms = document.getElementsByClassName('needs-validation');
-        var validation = Array.prototype.filter.call(forms, function (form) {
-            form.addEventListener('submit', function (event) {
-                if (form.checkValidity() === false) {
-                    event.preventDefault();
-                    event.stopPropagation();
-                }
-                form.classList.add('was-validated');
-            }, false);
-        });
-    }, false);
-})();
+    Array.from(forms).forEach(form => {
+        form.addEventListener('submit', checkErrorMessage);
+        form.addEventListener('submit', event => {
+            if (!form.checkValidity()) {
+                event.preventDefault()
+                event.stopPropagation()
+            }
+
+            form.classList.add('was-validated')
+        }, false)
+
+    })
+})()
