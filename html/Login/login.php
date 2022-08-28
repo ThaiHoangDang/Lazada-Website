d59ec27133be3bd0cbbc3ab01b1e22d15f7a6399
Dau Hoang Minh <s3926201@rmit.edu.vn>
2022-08-28 11:28:50 +0700

    login php

--- html/Login/login.php
+++ html/Login/login.php
@@ -1,14 +1,26 @@
+<?php
+session_start();
+
+if (isset($_POST['act'])) {
+    if (isset($_POST['pass']) && $_POST['pass'] == 'password135') {
+        $_SESSION['username'] = $_POST['username'];
+        header('location: ../Homepage/homepage.html');
+    }
+    $status = 'Invalid username/password';
+}
+?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
     <link rel="stylesheet" href="../../css/Profile/profile.css">
 </head>
+
 <body class="bg-light">
     <main class="page-content">
         <div class="container">
@@ -25,12 +37,19 @@
                             <label for="pass">Password</label>
                         </div>
                         <button class="mt-3 w-75 btn btn-lg btn-primary" type="submit" name="act">Sign in</button>
-                        <div class="mt-2">
+                        <div class="mt-2 mb-3">
                             <a href="../Register/pickRegister.html" class="link-secondary">Sign up for new account</a>
                         </div>
+
+                        <?php
+                        if (isset($status)) {
+                            echo "<h4 class=\"text-danger\">$status</h4>";
+                        }
+                        ?>
                 </div>
             </div>
         </div>
     </main>
 </body>
+
 </html>
