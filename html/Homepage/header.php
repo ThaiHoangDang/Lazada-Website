01a70ccae11555fce07a4f198836226232bdcf12
Your Namethaihoangdang <thaihoangdang1803@gmail.com>
2022-09-01 12:14:31 +0700

    fix bug

--- html/Homepage/header.php
+++ html/Homepage/header.php
@@ -10,7 +10,7 @@
   <body>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
       <div class="container-fluid">
-        <img src="../../img/lazadaLogoNoBG.svg" id="logoimg" alt="logo">
+        <img src="/img/lazadaLogoNoBG.svg" id="logoimg" alt="logo">
         <h2 class="logo">Lazada</h2>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
