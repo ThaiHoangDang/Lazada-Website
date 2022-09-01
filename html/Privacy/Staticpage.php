f1dd0c0bd2acc89a9f7e295f766314c36b2f2cda
Dau Hoang Minh <s3926201@rmit.edu.vn>
2022-09-01 16:06:49 +0700

    minor

---
+++ html/Privacy/Staticpage.php
@@ -0,0 +1,39 @@
+<?php
+include("../Homepage/header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="/css/staticpage/staticpage.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+  <!-- Source: https://pages.lazada.vn/wow/gcp/route/lazada/vn/upr_1000345_lazada/channel/vn/upr-router/vn?spm=a2o4n.home.footer_top.12.1905e1820pAvYb&hybrid=1&data_prefetch=true&prefetch_replace=1&at_iframe=1&wh_pid=/lazada/channel/vn/legal/privacy -->
+  <title>Document</title>
+</head>
+
+<body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
+  <main>
+    <div class="contain">
+      <h1>Introduction to privacy</h1>
+      <div class="content">
+        <p>
+          We may share your personal data with our third party service providers or affiliates (e.g. payment service providers) in order for them to offer their services to you in conjunction with your use of the Platform or our Services. Your acceptance and use of the third party service provider’s or our affiliate’s services shall be subject to terms and conditions as may be agreed between you and the third party service provider or our affiliate. Upon your acceptance of the third party service provider’s or our affiliate’s service offering, the collection, use, disclosure, storage, transfer and processing of your data (including your personal data and any data disclosed by us to such third party service provider or affiliate) shall be subject to the applicable privacy policy of the third party service provider or our affiliate, which shall be the data controller of such data. You agree that any queries or complaints relating to your acceptance or use of the third party service provider’s or our affiliate’s services shall be directed to the party named in the applicable privacy policy.
+        </p>
+        <p>
+          You may request information about your personal data which we have collected, or enquire about the ways in which your personal data may have been used, disclosed, stored or processed by us via the personal account information setting on our Platform or by contacting us using the contact details in Section 10 below. You may also request correction of any error or omission in your personal data which we have collected in the same way. In order to facilitate processing of your request, it may be necessary for us to request further information relating to your request. Where permissible under law, we may refuse such correction requests if deemed vexatious or unreasonable.
+        </p>
+      </div>
+    </div>
+  </main>
+</body>
+
+</html>
+
+<?php
+include("../Homepage/footer.php");
+?>
