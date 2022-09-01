632f0af01e35f90b6563749e4a055a8de5e4069e
Your Namethaihoangdang <thaihoangdang1803@gmail.com>
2022-09-01 07:00:31 +0700

    change css name

--- html/Vendor/vendorHomepage.php
+++ html/Vendor/vendorHomepage.php
@@ -62,7 +62,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Cart</title>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
-        <link rel="stylesheet" href="../../css/homepage/homepageTest2.css">
+        <link rel="stylesheet" href="../../css/homepage/homepage.css">
         <link rel="stylesheet" href="/css/Vendor/vendorpage.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
     </head>
