0fe03ce09421b923505337304372adc649786bf2
Your Namethaihoangdang <thaihoangdang1803@gmail.com>
2022-09-04 19:10:29 +0700

    add Favicon

--- html/Privacy/personalprotect.php
+++ html/Privacy/personalprotect.php
@@ -10,6 +10,7 @@ session_start();
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Personal information protection commitment</title>
+  <link rel="icon" type="image/x-icon" href="/img/lazadaLogo.webp">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
   <link rel="stylesheet" href="/css/homepage/header-footer.css">
   <link rel="stylesheet" href="/css/staticpage/staticpage.css">
